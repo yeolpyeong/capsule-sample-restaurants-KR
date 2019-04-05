df00033af02217158577a046cbc98f864c1cefc1
j-peter-jeon <j_peter.jeon@samsung.com>
2019-04-05 14:18:31 +0900

    Update: Change Scenario

--- code/makeCandidateRestaurant.js
+++
@@ -1,9 +0,0 @@
-//다른 경로의 파일을 가져옵니다.
-var fakeData = require("./data/restaurants.js");
-
-module.exports.function = function makeCandidateRestaurant() {
- 
-  console.log(fakeData);
-  
-  return fakeData;
-}
